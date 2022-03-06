4c7bd058ba0653fca27c53d7e1490e998ad4e983
Sebastian Young Ye <yye3028@gmail.com>
2022-03-06 18:56:10 +1100

    Add project files.

---
+++ HollowKnight.DisableRingingItemSound/DisableRingingItemSound.cs
@@ -0,0 +1,25 @@
+﻿using Modding;
+using UnityEngine;
+
+namespace HollowKnight.DisableRingingItemSound
+{
+    public class DisableRingingItemSound : Mod
+    {
+        public override string GetVersion() => "1.0.0";
+
+        public override void Initialize()
+        {
+            On.PlayMakerFSM.OnEnable += PlayMakerFSM_OnEnable;
+        }
+
+        private void PlayMakerFSM_OnEnable(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self)
+        {
+            orig(self);
+
+            if (self.FsmName == "Shiny Control")
+            {
+                self.gameObject.GetComponent<AudioSource>()?.Stop();
+            }
+        }
+    }
+}
